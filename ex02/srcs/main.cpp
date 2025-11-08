285663df4c2c30d8575d3fa0e4ba37def6bab3d1
jramiro <jramiro@student.42paris.fr>
2025-11-08 22:31:45 +0100

    ex02 inprog

---
+++ ex02/srcs/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+#include "colors.hpp"
+#include "dictionary.hpp"
+
+// --- helper functions declaration ---
+
+// --- main function ---
+int main(int ac, char** av)
+{
+	(void)ac;
+	(void)av;
+
+	return (OK);
+}
+
+
+
+
+
+// --- helper functions definition ---
