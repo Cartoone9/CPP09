68f6e8fdafa28fe03372a481c90c62bf711b3121
jramiro <jramiro@student.42paris.fr>
2025-11-20 19:19:21 +0100

    almost done

--- ex02/srcs/main.cpp
+++ ex02/srcs/main.cpp
@@ -1,5 +1,8 @@
 #include <algorithm>
+#include <ctime>
+#include <deque>
 #include <iostream>
+#include <iomanip>
 #include <sstream>
 #include <vector>
 
@@ -13,9 +16,11 @@
 // --- helper functions declaration ---
 static void	invalidUsage();
 static void	invalidArg();
-static int	parseArgs(int ac, char** av, std::vector<int>& temp_vec);
+static int	parseArgs(int ac, char** av,
+					 std::vector<int>& base_vec, std::deque<int>& base_deq);
 static bool	checkDigits(char* str);
 static void	vectorFordJohnson(std::vector<int>& temp_vec);
+static void	dequeFordJohnson(std::deque<int>& base_deq);
 static int	findJacobsthal(int n);
 
 // --- main function ---
@@ -28,21 +33,36 @@ int main(int ac, char** av)
 	}
 
 	std::vector<int> base_vec;
+	std::deque<int> base_deq;
 
-	if (parseArgs(ac, av, base_vec) == ERROR)
+	if (parseArgs(ac, av, base_vec, base_deq) == ERROR)
 		return (NOK);
 
-	// DEBUG
-	std::cout << "Before:" << std::endl;
-	for (size_t i = 0; i < base_vec.size(); i++)
-		std::cout << "Value [" << i << "]: " << base_vec[i] << std::endl;
+	// VECTOR	
+	std::clock_t start_time = std::clock();
 
 	vectorFordJohnson(base_vec);
 
-	// DEBUG
-	std::cout << "After:" << std::endl;
-	for (size_t i = 0; i < base_vec.size(); i++)
-		std::cout << "Value [" << i << "]: " << base_vec[i] << std::endl;
+	std::clock_t end_time = std::clock();
+	double	duration_ticks = static_cast<double>(end_time - start_time);
+	double	duration_seconds = duration_ticks / CLOCKS_PER_SEC;
+	std::cout << "Time to process a range of "
+		<< base_vec.size() << " with std::vector: "
+		<< std::fixed << std::setprecision(6)
+		<< duration_seconds << " s." << std::endl;
+
+	// DEQUE
+	start_time = std::clock();
+
+	dequeFordJohnson(base_deq);
+
+	end_time = std::clock();
+	duration_ticks = static_cast<double>(end_time - start_time);
+	duration_seconds = duration_ticks / CLOCKS_PER_SEC;
+	std::cout << "Time to process a range of "
+		<< base_vec.size() << " with std::deque: "
+		<< std::fixed << std::setprecision(6)
+		<< duration_seconds << " s." << std::endl;
 
 	return (OK);
 }
@@ -62,7 +82,8 @@ static void	invalidArg()
 	std::cerr << RED "Error: " RESET << "invalid arg in given values" << std::endl;
 }
 
-static int	parseArgs(int ac, char** av, std::vector<int>& base_vec)
+static int	parseArgs(int ac, char** av,
+					 std::vector<int>& base_vec, std::deque<int>& base_deq)
 {
 	for (int i = 1; i < ac; i++)
 	{
@@ -78,6 +99,7 @@ static int	parseArgs(int ac, char** av, std::vector<int>& base_vec)
 		while (iss >> temp_int)
 		{
 			base_vec.push_back(temp_int);
+			base_deq.push_back(temp_int);
 		}
 	}
 	return (OK);
@@ -214,6 +236,126 @@ static void	vectorFordJohnson(std::vector<int>& base_vec)
 	}
 
 	base_vec = main_chain;
+}
+
+static void	dequeFordJohnson(std::deque<int>& base_deq)
+{
+	size_t	base_deq_len = base_deq.size();
+
+	if (base_deq_len <= 1)
+		return;
+
+	bool	is_odd = false;
+	int		straggler;
+
+	// odd number of elements -> straggler
+	if (base_deq_len % 2 != 0)
+	{
+		is_odd = true;
+		straggler = base_deq[base_deq_len - 1];
+		base_deq.pop_back();
+		base_deq_len--;
+	}
+
+	std::deque<std::pair<int, int> >	orig_pairs;
+	std::deque<int>						winners;
+
+	// form the orig_pairs, preorder them so that
+	// first is always the biggest of the pair
+	// and keep a deque of winners aside for recursion
+	for (size_t i = 0; i < base_deq_len; i += 2)
+	{
+		int	first = base_deq[i];
+		int	second = base_deq[i + 1];
+
+		if (first > second)
+		{
+			orig_pairs.push_back(std::make_pair(first, second));
+			winners.push_back(first);
+		}
+		else
+		{
+			orig_pairs.push_back(std::make_pair(second, first));
+			winners.push_back(second);
+		}
+	}
+
+	dequeFordJohnson(winners);
+
+	std::deque<int>					main_chain = winners;
+	std::deque<std::pair<int, int> >	sorted_pairs;
+
+	for (size_t i = 0; i < main_chain.size(); ++i)
+	{
+		int current_winner = main_chain[i];
+		// Find the loser corresponding to this winner.
+		for (size_t j = 0; j < orig_pairs.size(); ++j)
+		{
+			if (orig_pairs[j].first == current_winner)
+			{
+				sorted_pairs.push_back(orig_pairs[j]);
+				// remove the found pair
+				orig_pairs.erase(orig_pairs.begin() + j);
+				break;
+			}
+		}
+	}
+
+	std::deque<std::pair<int, size_t> >	pend_chain; // { loser, winner_index }
+
+	for (size_t i = 0; i < main_chain.size(); i++)
+	{
+		pend_chain.push_back(std::make_pair(sorted_pairs[i].second, i + 1)); // shift index by 1
+																	  // to account for the first
+																	  // pend element insertion
+	}
+
+	// insert pend into main here
+	// first start with the smallest element of the losers
+	// which goes at the beginning
+	main_chain.insert(main_chain.begin(), pend_chain[0].first);
+
+	int	index_jacob = 3;
+	int	curr_jacob = findJacobsthal(index_jacob++);
+	int	prev_jacob = 1;
+
+	while (static_cast<size_t>(prev_jacob) < pend_chain.size())
+	{
+		if (static_cast<size_t>(curr_jacob) > pend_chain.size())
+			curr_jacob = pend_chain.size();
+
+		for (int i = curr_jacob; i > prev_jacob; i--)
+		{
+			int		value = pend_chain[i - 1].first;
+			size_t	limit_index = pend_chain[i - 1].second;
+
+			std::deque<int>::iterator	limit_it = main_chain.begin() + limit_index;
+			std::deque<int>::iterator	pos = std::lower_bound(main_chain.begin(), limit_it, value);
+
+			size_t	insertion_index = std::distance(main_chain.begin(), pos);
+
+			main_chain.insert(pos, value);
+
+			std::deque<std::pair<int, size_t> >::iterator it;
+			std::deque<std::pair<int, size_t> >::iterator it_end = pend_chain.end();
+			for (it = pend_chain.begin(); it != it_end; it++)
+			{
+				if (it->second >= insertion_index)
+					it->second++;
+			}
+		}
+		prev_jacob = curr_jacob;
+		curr_jacob = findJacobsthal(index_jacob++);
+	}
+
+	// if is_odd insert straggler here
+	if (is_odd)
+	{
+		std::deque<int>::iterator pos = std::lower_bound(main_chain.begin(), main_chain.end(), straggler);
+		main_chain.insert(pos, straggler);
+	}
+
+	base_deq = main_chain;
 }
 
 static int	findJacobsthal(int n)
